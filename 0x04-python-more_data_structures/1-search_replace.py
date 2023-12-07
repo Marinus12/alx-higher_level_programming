a36e1d87283cb27d042d7f642eba7982d73c14ca
Ukewuihe Marinus <dubemarinus@gmail.com>
2023-12-07 05:07:53 -0800

    Python - More Data Structures: Set Dictionary

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, serach, replace):
-    return (replace if search == n else n for n in my_list)
+    return [replace if search == n else n for n in my_list]
