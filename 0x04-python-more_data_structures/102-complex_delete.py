f2ed54534401618f0c8efa20992516c628a783ec
Ukewuihe Marinus <dubemarinus@gmail.com>
2023-12-07 05:53:53 -0800

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,5 +3,5 @@ def complex_delete(a_dictionary, value):
     tmp = a_dictionary.copy()
     for j, v in tmp.items():
         if value == v:
-            a_dictionary(j)
+            a_dictionary.pop(j)
     return a_dictionary
