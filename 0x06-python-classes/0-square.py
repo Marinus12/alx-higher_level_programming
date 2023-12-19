e000072222a047d618b6602d61f096ab9ab70587
Ukewuihe Marinus <dubemarinus@gmail.com>
2023-12-19 14:40:23 -0800

    Python - Classes and Objects

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A module of square"""
+
+
+class Square:
+    """Represent a square"""
+    pass
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pyhton3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a new square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
