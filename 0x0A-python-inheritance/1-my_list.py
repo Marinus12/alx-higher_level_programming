65a27dcce039b816147d7ac7f0d1610a600deeef
Ukewuihe Marinus <dubemarinus@gmail.com>
2024-01-08 14:09:08 -0800

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class MyList that inherits from list."""
+
+
+def MyList(list):
+    """Implements sorted printing for the built-in list class."""
+
+    def printed_sorted(self):
+        """Prints a list in sorted ascending order."""
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initializes a new rectangle.
+
+        Args:
+            width (int): The width of the rectangle
+            height (int): The height of the rectangle.
+        """
+        swlf.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
