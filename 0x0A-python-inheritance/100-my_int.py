d7f81031b6b7d74cad7e13923ffecdc1db9df114
Ukewuihe Marinus <dubemarinus@gmail.com>
2024-01-09 02:35:05 -0800

    0x0A. Python - Inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+i#!/usr/bin/python3
+"""Defines a class MyList that inherits from int."""
+
+
+class MyInt(int):
+    """Invert int operators == and !=."""
+
+    def __eq__(self, value):
+        """Override == operators with != behaviour."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behaviour."""
+        return self.real == value
