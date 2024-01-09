546465f1abfc49e4b2d78bc1290d0e0885d216e5
Ukewuihe Marinus <dubemarinus@gmail.com>
2024-01-09 13:52:44 -0800

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -17,25 +17,25 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, atts=None):
+    def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
 
-        If attrs is a list of strings, represents only those sttributes
+        If attrs is a list of strings, represents only those attributes
         included in the list.
 
         Args:
-            attr (list): (Optional) The attributes to represent.
+            attrs (list): (Optional) The attributes to represent.
         """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
-            return (k: getattr(self, k) for k in attrs if hasattr(self, k))
-            return self.__dict__
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
-        def reload_from_json(self, json):
-            """Replace all attributes of the Student.
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student.
 
-            Args:
-                json (dict): The key/value pairs to replace attributes with.
-            """
-            for k, v in json.items():
-                setattr(self, k, v)
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -11,11 +11,11 @@ class Student:
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
-            age (int): The age of the student/
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
-        self.age
+        self.age = age
 
     def to_json(self):
         """Get a dictionary representation of the Student."""
