0be1e119dae50db236c6d64315856376de59057e
Ukewuihe Marinus <dubemarinus@gmail.com>
2024-02-15 11:20:49 -0800

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
 -- Script that creates the table force_name on my MySQL derver
 -- id INT
 -- name VARCHAR(256) can't be be null
-CREATE TABLE IF NOT EXISTS force_name (Id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
 -- Script that creates the table unique_id on my MySQL server.
 -- id INT with default value 1 and must be unique
 -- name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS unique_id (Id INT DEFAULT 1 UNIQUE, VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, VARCHAR(256));
