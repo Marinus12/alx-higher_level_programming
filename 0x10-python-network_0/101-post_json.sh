c5ca0e04483ca6714a7c9bf1f23adaf298653697
Ukewuihe Marinus <dubemarinus@gmail.com>
2024-03-27 08:53:23 -0700

    New commits

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to URL passed as the argument as the first argument
-curl -s -X -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
