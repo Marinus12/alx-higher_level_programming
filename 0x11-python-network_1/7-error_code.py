9e7bf955ef54be9fccf3726316bcf1f91eed4b14
Ukewuihe Marinus <dubemarinus@gmail.com>
2024-03-29 02:51:25 -0700

    New commits

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-""" Script that takes in a URL, sends a request to the URL and displays
-    the body of the request of the response
+"""Sends a request to a given URL and displays the response body.
+
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
 """
-import requests
 import sys
+import requests
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     url = sys.argv[1]
-    reponse = requests.get(url)
 
-    if response.status_code >= 400:
-        print("Error code: {}".format(response.status_code))
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print(response.text)
+        print(r.text)
